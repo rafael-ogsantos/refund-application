e9eab57d2acf7b6ffde7d5218cc89f3d50c4fbe1
rafael-ogsantos <rafaelogsantos@gmail.com>
2021-12-02 12:59:20 -0300

    first commit

---
+++ app/Console/Commands/ImportCities.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Outsource\ApiLocales;
+use Illuminate\Console\Command;
+
+class ImportCities extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:cities';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import cities from my state';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $cities = (new ApiLocales)->getCities();
+
+        foreach($cities as $city) {
+            dump($city['nome']);
+        }
+    }
+}
---
+++ database/migrations/2021_10_26_020254_create_enrollment_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnrollmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('course_id');
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enrollment');
+    }
+}
